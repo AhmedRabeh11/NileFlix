aa2e032fd4c7a68d5a69fed26611b7006997e862
= <=>
2024-10-12 00:39:02 +0200

    Initial commit - NileFlix Backend and Frontend setup

---
+++ backend/src/main/java/com/nileflix/reviews/NileFlixApplication.java
@@ -0,0 +1,13 @@
+package com.nileflix.reviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NileFlixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NileFlixApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/nileflix/reviews/NileFlixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nileflix.reviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NileFlixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
