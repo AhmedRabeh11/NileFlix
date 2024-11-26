4459ec9301ab1a1ae11e637c7f0cae513863c920
= <=>
2024-11-26 14:07:51 +0100

    Movie Service Added

--- backend/src/main/java/com/nileflix/service/MovieService.java
+++ backend/src/main/java/com/nileflix/service/MovieService.java
@@ -1,4 +1,33 @@
 package com.nileflix.service;
 
+import com.nileflix.model.Movie;
+import com.nileflix.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class MovieService {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public List<Movie> getAllMovies() {
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> getMovieById(Long id) {
+        return movieRepository.findById(id);
+    }
+
+    public List<Movie> searchMovies(String title) {
+        return movieRepository.findByTitleContaining(title);
+    }
+
+    public Movie saveMovie(Movie movie) {
+        return movieRepository.save(movie);
+    }
 }
+
