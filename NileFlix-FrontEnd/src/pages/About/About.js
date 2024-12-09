dc8f9cf78170fb5fe7cc9317cba0847da05e07b7
Ahmed Hisham <ahmedhisham1928@gmail.com>
2024-12-09 20:28:40 +0100

    modified the About page

--- NileFlix-FrontEnd/src/pages/About/About.js
+++ NileFlix-FrontEnd/src/pages/About/About.js
@@ -5,35 +5,35 @@ const About = () => {
     return (
         <div className="about-container">
             <h2>About Us</h2>
-            <p>Welcome to NileFlix ! We are dedicated to bringing you the best movies and TV shows from around the world</p>
+            <p>Welcome to NileFlix! We are dedicated to bringing you the best movies and TV shows from around the world.</p>
             
             <section className="mission">
                 <h3>Our Mission</h3>
-                <p>Our mission is to provide a seamless and enjoyable streaming experience for our users We strive to offer a diverse selection of content that caters to all tastes and preferences.</p>
+                <p>Our mission is to provide a seamless and enjoyable streaming experience for our users. We strive to offer a diverse selection of content that caters to all tastes and preferences.</p>
             </section>
             
             <section className="team">
                 <h3>Meet the Team</h3>
                 <div className="team-members">
                     <div className="team-member">
-                        <img src="path/to/ahmed-hisham.jpg" alt="Ahmed Hisham" />
-                        <h4>AhmedHisham</h4>
-                        <p>Front-End Develeoper</p>
+                        <img src="/images/ahmed.JPG" alt="Ahmed Hisham" />
+                        <h4>Ahmed Hisham</h4>
+                        <p>Front-End Developer</p>
                     </div>
                     <div className="team-member">
-                        <img src="path/to/salah-sultan.jpg" alt="Salah Sultan" />
+                        <img src="/images/salah.jpg" alt="Salah Sultan" />
                         <h4>Salah Sultan</h4>
-                        <p>Unit Tester </p>
+                        <p>Unit Tester</p>
                     </div>
                     <div className="team-member">
-                        <img src="path/to/ahmed-rabeh.jpg" alt="Ahmed Rabeh" />
+                        <img src="/images/rabeh.jpg" alt="Ahmed Rabeh" />
                         <h4>Ahmed Rabeh</h4>
-                        <p>Backend Develeoper</p>
+                        <p>Backend Developer</p>
                     </div>
                     <div className="team-member">
-                        <img src="path/to/mohamed-abdallah.jpg" alt="Mohamed Abdallah" />
+                        <img src="/images/mo.jpg" alt="Mohamed Abdallah" />
                         <h4>Mohamed Abdallah</h4>
-                        <p>Database Develeoper</p>
+                        <p>Database Developer</p>
                     </div>
                 </div>
             </section>
@@ -43,8 +43,8 @@ const About = () => {
                 <p>If you have any questions or feedback, feel free to reach out to us:</p>
                 <ul>
                     <li>Email: support@nileflix.com</li>
-                    <li>Phone: +36 70 947 645 7 </li>
-                    <li>Address: Debrecen Hungart , 4026 Csemete Utca</li>
+                    <li>Phone: +36 70 947 645 7</li>
+                    <li>Address: Debrecen, Hungary, 4026 Csemete Utca</li>
                 </ul>
             </section>
         </div>
