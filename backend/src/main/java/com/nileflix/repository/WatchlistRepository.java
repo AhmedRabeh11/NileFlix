7759920872c52661138396a0b598b9466378497d
SalahSultan <salah.11301@outlook.com>
2024-12-09 23:04:51 +0100

    modified watchlist repo

--- backend/src/main/java/com/nileflix/repository/WatchlistRepository.java
+++ backend/src/main/java/com/nileflix/repository/WatchlistRepository.java
@@ -4,10 +4,7 @@ import com.nileflix.model.Watchlist;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface WatchlistRepository extends JpaRepository<Watchlist, Long> {
-    List<Watchlist> findByUserId(String userId);
-    void deleteByUserIdAndMovie_MovieId(String userId, Long movieId);
+    void deleteByMovie_MovieId(Long movieId);
 }
