f39cc0b87cb77d2af67034d2a8cec0b9a2138937
mohamedworkbot <mohamed.workemail2003@gmail.com>
2024-12-10 00:10:22 +0100

    made the reviewscontext use the user.id

--- NileFlix-FrontEnd/src/pages/Context/ReviewsContext.js
+++ NileFlix-FrontEnd/src/pages/Context/ReviewsContext.js
@@ -1,5 +1,6 @@
 // src/context/ReviewsContext.js
 import React, { createContext, useContext, useState } from 'react';
+import { useAuth } from './AuthContext';
 
 const ReviewsContext = createContext();
 
@@ -7,11 +8,13 @@ export const useReviews = () => useContext(ReviewsContext);
 
 export const ReviewsProvider = ({ children }) => {
     const [reviews, setReviews] = useState({});
+    const { user } = useAuth();
 
     const addReview = (movieId, review) => {
+        const reviewWithAuthor = { ...review, author: user.username };
         setReviews((prevReviews) => ({
             ...prevReviews,
-            [movieId]: [...(prevReviews[movieId] || []), review],
+            [movieId]: [...(prevReviews[movieId] || []), reviewWithAuthor],
         }));
     };
 
