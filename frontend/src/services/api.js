bc817a956c32dc3cfc3969ecbaa0b77813ee22a9
mohamedworkbot <mohamed.workemail2003@gmail.com>
2024-12-09 15:07:26 +0100

    added nodemodules to gitignore

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_BASE_URL = 'http://localhost:8080/api'; // Your Spring Boot backend URL
 
 const username = 'user'; // Replace with your username
-const password = 'c7193b72-53cf-4a59-a5d0-b46894ce2055'; // Replace with the password provided by Spring Boot
+const password = '731a46e8-6ddb-494b-9ffe-801fbcc3fbf1'; // Replace with the password provided by Spring Boot
 
 const authHeader = `Basic ${btoa(`${username}:${password}`)}`;
 
